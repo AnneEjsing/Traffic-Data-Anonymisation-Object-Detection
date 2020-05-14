f196cb74254847be7b9662384361272c23a4ff1f
Jacob Nørhave <jnarha16@student.aau.dk>
2020-05-14 09:53:13 +0200

    Fix tensorflow model name for changing models

--- tensorflow_ssd/detection.py
+++ tensorflow_ssd/detection.py
@@ -155,10 +155,10 @@ def generate_frames(license_plate_model, face_model):
 
     while success and not stop.is_set():
         # If a new model is present, excahnge this with the old model.
-        if "new_face.pb" in os.listdir(face_model_dir):
+        if "new_face_model.pb" in os.listdir(face_model_dir):
             sp.call(f"mv {face_model_dir}/new_face_model.pb {face_model_dir}/saved_model.pb", shell=True)
             model = load_model()
-        if "new_license_plate.pb" in os.listdir(license_plate_model_dir):
+        if "new_license_plate_model.pb" in os.listdir(license_plate_model_dir):
             sp.call(f"mv {license_plate_model_dir}/new_license_plate_model.pb {license_plate_model_dir}/saved_model.pb", shell=True)
             model = load_model()
 
